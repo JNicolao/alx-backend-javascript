73c3098dcbac501b246404874bbf7841eb934e14
JNicolao <bororio96@gmail.com>
2023-09-26 16:28:07 +0300

    constant.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    const task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow
+    const task2 = false;
   }
 
   return [task, task2];
