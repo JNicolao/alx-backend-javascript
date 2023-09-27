db4da109b1530f715d6a914da435a778d1c485bf
JNicolao <bororio96@gmail.com>
2023-09-27 14:24:37 +0300

    lint

--- 0x01-ES6_promise/.eslintrc.js
+++ 0x01-ES6_promise/.eslintrc.js
@@ -4,25 +4,25 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  extends: ["airbnb-base", "plugin:jest/all"],
+  extends: ['airbnb-base', 'plugin:jest/all'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["jest"],
+  plugins: ['jest'],
   rules: {
-    "no-console": "off",
-    "no-shadow": "off",
-    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
   },
   overrides: [
     {
-      files: ["*.js"],
-      excludedFiles: "babel.config.js",
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
     },
   ],
 };
--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -1,13 +1,13 @@
 export function uploadPhoto() {
   return Promise.resolve({
     status: 200,
-    body: "photo-profile-1",
+    body: 'photo-profile-1',
   });
 }
 
 export function createUser() {
   return Promise.resolve({
-    firstName: "Guillaume",
-    lastName: "Salva",
+    firstName: 'Guillaume',
+    lastName: 'Salva',
   });
 }
