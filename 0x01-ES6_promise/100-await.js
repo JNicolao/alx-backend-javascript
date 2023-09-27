2c66a4b99024dc31da096fd44e3c3853960523c9
JNicolao <bororio96@gmail.com>
2023-09-27 14:00:14 +0300

    await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,11 +2,8 @@ import { uploadPhoto, createUser } from "./utils";
 
 export default async function asyncUploadUser() {
   try {
-    return {
-      photo: await uploadPhoto(),
-      user: await createUser(),
-    };
-  } catch (_) {
+    return { photo: await uploadPhoto(), user: await createUser() };
+  } catch (e) {
     return { photo: null, user: null };
   }
 }
