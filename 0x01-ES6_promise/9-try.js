e0a921cb17a9e0bb335d703a4591b521021293d7
JNicolao <bororio96@gmail.com>
2023-09-27 14:05:45 +0300

    try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,11 +1,13 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+
   try {
     queue.push(mathFunction());
-  } catch (error) {
-    queue.push(error.toString());
+  } catch (err) {
+    queue.push(String(err));
   } finally {
     queue.push("Guardrail was processed");
   }
+
   return queue;
 }
