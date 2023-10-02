ab821f2bcc5d2f5e0df8ebafde63ef652b96b774
JNicolao <bororio96@gmail.com>
2023-10-02 14:45:16 +0300

    get list students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents(){
+    const student1 = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const student2 = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const student3 = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+    return [student1, student2, student3];
+}
