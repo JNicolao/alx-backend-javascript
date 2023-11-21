9815b70f1cb2946d8614dc2c8d59a65ea70bd705
JNicolao <bororio96@gmail.com>
2023-11-21 21:13:53 +0300

    Node.js Basics

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,12 @@
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+
+const mapRoutes = (app) => {
+  app.get('/', AppController.getHomepage);
+  app.get('/students', StudentsController.getAllStudents);
+  app.get('/students/:major', StudentsController.getAllStudentsByMajor);
+};
+
+export default mapRoutes;
+module.exports = mapRoutes;
